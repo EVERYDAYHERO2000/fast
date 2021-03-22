5698fe3695f05870343b09eb07da664cb6c7bd78
Ilya Aralin <aralinilya@yandex-team.ru>
2021-03-22 23:39:35 +0300

    fix

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const UglifyJS = require("uglify-js");
 const fs = require('fs-extra');
 
-fs.readFile('./fast/fast.js', 'utf8' , (err, data) => {
+fs.readFile('./src/fast.js', 'utf8' , (err, data) => {
   if (err) console.error(err);
   
   const result = UglifyJS.minify(data).code; 
